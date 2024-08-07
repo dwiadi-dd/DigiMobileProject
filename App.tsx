2a968903978a885b258ced35229d5d937b8e5bb6
Dwi Adi Bangkit <dwi.bangkit@djembatandua.com>
2024-08-07 22:29:01 +0700

    CODEPUSH IMPLEMENT

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@
  *
  * @format
  */
-
 import React, {FC, useEffect} from 'react';
 import MainNavigation from '@navigations/index';
 import messaging from '@react-native-firebase/messaging';
@@ -13,24 +12,38 @@ import analytics from '@react-native-firebase/analytics';
 import crashlytics from '@react-native-firebase/crashlytics';
 import JailMonkey from 'jail-monkey';
 import {Alert} from 'react-native';
+import codePush from 'react-native-code-push';
 
 if (__DEV__) {
   analytics().setAnalyticsCollectionEnabled(true);
 }
+
 async function onMessageReceived(message: any) {
   notifee.displayNotification(JSON.parse(message.data.notifee));
 }
 
 messaging().onMessage(onMessageReceived);
 messaging().setBackgroundMessageHandler(onMessageReceived);
+
 const App: FC = () => {
   useEffect(() => {
     crashlytics().log('App mounted.');
-
     if (JailMonkey.isJailBroken()) {
       Alert.alert('Warning', 'This device is jailbroken/rooted.');
     }
+
+    codePush.sync({
+      installMode: codePush.InstallMode.ON_NEXT_RESTART,
+      mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
+    });
   }, []);
+
   return <MainNavigation />;
 };
-export default App;
+
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_START,
+  installMode: codePush.InstallMode.ON_NEXT_RESTART,
+};
+
+export default codePush(codePushOptions)(App);
--- android/app/src/main/java/com/digimobileproject/MainApplication.kt
+++ android/app/src/main/java/com/digimobileproject/MainApplication.kt
@@ -10,7 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-
+import com.microsoft.codepush.react.CodePush
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -24,7 +24,9 @@ class MainApplication : Application(), ReactApplication {
         override fun getJSMainModuleName(): String = "index"
 
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
+        override fun getJSBundleFile(): String {
+              return CodePush.getJSBundleFile() 
+          }
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
